babc6960db227ae11ecd4b2e2ca407f278f0ade0
meltyblend <aren.jpg@gmail.com>
2024-12-05 22:49:11 -0800

    simple vector addition to start

--- main.cpp
+++ main.cpp
@@ -19,11 +19,19 @@
 using namespace std;
 int main() {
 
-    vector<int> v = {1,2,3,4,5,6,7,8,9};
+    vector<int> v1 = {1,2,3,4,5,6,7,8,9};  //declaring vecotr 1
+    vector<int> v2 = {9,8,7,6,5,4,3,2,1};  // declaring vector 2
+    vector<int> v3;                        // creating a new space for the sum of v1 + v2
+    v3.reserve(v1.size());
 
-    for(int i=0;i<v.size();i++) {
-        cout<<v[i]<<" ";
+    for (size_t i = 0; i < v1.size(); i++) {   // vector addition process
+        v3.push_back(v1[i] + v2[i]);
     }
+
+    for (auto i : v3) {                    // printing out v3 after being summed
+        cout << i << " ";
+    }
+
 
     return 0;
 }
