a0710dc97e076708dd99a2c598130c8b4aff4bff
Aren <106656925+meltyblend@users.noreply.github.com>
2024-12-20 13:24:26 -0800

    In Progress: Dot product function
    Completed:
    Addition of vectors
    subtraction of vectors

--- main.cpp
+++ main.cpp
@@ -17,21 +17,88 @@
 #include <cstdlib>
 
 using namespace std;
+
+vector<float> Vec_Add(vector<float> v1, vector<float> v2);
+vector<float> Vec_Sub(vector<float> v1, vector<float> v2);
+
+
 int main() {
 
-    vector<int> v1 = {1,2,3,4,5,6,7,8,9};  //declaring vecotr 1
-    vector<int> v2 = {9,8,7,6,5,4,3,2,1};  // declaring vector 2
-    vector<int> v3;                        // creating a new space for the sum of v1 + v2
-    v3.reserve(v1.size());
+    vector<float> v1 = {1,2,3,4,5,6,7,8,9};
+    vector<float> v2 = {1,2,3,4,5,6,7,8,9};
 
-    for (size_t i = 0; i < v1.size(); i++) {   // vector addition process
-        v3.push_back(v1[i] + v2[i]);
-    }
+    cout << "adding vector 1 and vector 2 :" << endl;
+    vector<float> v3 = Vec_Add(v1, v2); // adding two vectors of same sizes
 
-    for (auto i : v3) {                    // printing out v3 after being summed
-        cout << i << " ";
-    }
+    cout << endl;
 
+    cout << "subtracting vector 1 and vector 2 :" << endl;
+    vector<float> v4 = Vec_Sub(v1, v2);
+
+    cout << endl;
 
     return 0;
 }
+
+// function to add two vectors
+vector<float> Vec_Add(vector<float> v1, vector<float> v2) {
+    if (v1.size() != v2.size()) {
+        cerr << "Vector_Add Error" << endl;                  // checking to make sure vectors are same size
+    }
+    vector<float> total;
+    for (auto i = 0; i < v1.size(); i++) {                  // summing vectors
+        total.push_back(v1[i] + v2[i]);
+    }
+
+    for(auto val: total) {                                 // printing vector sum
+        cout << val << " ";
+    }
+
+    cout <<  endl;
+
+    return total;
+}
+
+vector<float> Vec_Sub(vector<float> v1, vector<float> v2) {
+    if (v1.size() != v2.size()) {
+        cerr << "Vector_Add Error" << endl;                  // checking to make sure vectors are same size
+    }
+    vector<float> total;
+    for (auto i = 0; i < v1.size(); i++) {
+        total.push_back(v1[i] - v2[i]);                      // subtracting vectors
+    }
+
+    for(auto val: total) {                                // printing vector sum
+        cout << val << " ";
+    }
+
+    cout <<  endl;
+
+    return total;
+}
+
+/*  working on dot products of the code
+ *
+vector<float> Dot_Product(vector<float> v1, vector<float> v2) {
+    if (v1.size() != v2.size()) {
+        cerr << "Dot_Product Error" << endl;
+    }
+
+    int total = 0;
+    int multi = 0;
+    int add = 0;
+
+    for (auto i = 0; i < v1.size(); i++) {
+        for (auto j = 0; j < v2.size(); j++) {
+            multi = v1[i] * v2[j];
+            add = multi + multi;
+        }
+    }
+
+
+
+}
+*/
+
+
+
